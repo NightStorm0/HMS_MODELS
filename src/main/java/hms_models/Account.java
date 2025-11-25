07b013f115914ed0826f28fd7cd3b21109502359
NightStorm0 <itzgideonkane@gmail.com>
2025-11-26 00:18:24 +0100

    initialize project structure with .gitignore, main application files, and Maven configuration

---
+++ src/main/java/hms_models/Account.java
@@ -0,0 +1,13 @@
+package hms_models;
+
+public class Account {
+    public enum AccountType {
+        ADMIN,
+        DOCTOR,
+        NURSE,
+        FRONT_DESK,
+        PHARMACIST,
+        LAB,
+        ACCOUNTANT
+    }
+}
---
+++ src/main/java/pro/emr_hms/onezero/Main.java
@@ -0,0 +1,9 @@
+package pro.emr_hms.onezero;
+
+import hms_models.DatabaseObjects;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
